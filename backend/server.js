7f58ceba9f83867e67ce439576d7085b26885635
JacobBatson <jacobbatson123@aol.com>
2025-04-19 04:46:42 -0400

    fixing login

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,7 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 // ——————————————
 // 5) START SERVER
 // ——————————————
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
